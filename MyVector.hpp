538f8d325b8b3db6fbd9d464cc4c53e929280a0e
Emad Albalas <ealbalas@csu.fullerton.edu>
2022-12-19 19:49:01 -0800

    added more functions such as front, end, and swap. all compressed code for easier readibility

--- MyVector.hpp
+++ MyVector.hpp
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Creating a vector class. This will be a dynamically allocated array.
+
 template <typename T>
 class MyVector
 {
@@ -32,12 +34,9 @@ public:
     MyVector(const MyVector &other)
         : size_(other.size_),
           capacity_(other.capacity_),
-          elements_(new T[other.capacity]())
+          elements_(new T[other.capacity_]())
     {
-        for (int i = 0; i < other.size_; i++)
-        {
-            elements_[i] = other[i];
-        }
+        copyOther(other);
     }
     // Destructor
     ~MyVector()
@@ -54,11 +53,7 @@ public:
 		if (this != &rhs)
 		{
 			reserve(rhs.capacity());
-
-			for (size_t i = 0; i < rhs.size(); i++)
-            {
-				elements_[i] = rhs[i];
-			}
+			copyOther(rhs);
 			this->capacity_ = rhs.capacity();
 			this->size_ = rhs.size();
 			return *this;
@@ -89,15 +84,13 @@ public:
     // Check if vector is empty
     bool empty() const
     {
-        if (this->size_ == 0)
-            return true;
+        if (this->size_ == 0) return true;
     }
 
     // Get a value at given index
     T &at(size_t index) const
     {
-        if (index > this->size_)
-            throw std::range_error("Gave index out of range.");
+        if (index > this->size_) throw std::range_error("Gave index out of range.");
         T &value = this->elements_[index];
         return value;
     }
@@ -114,7 +107,7 @@ public:
             }
             this->elements_ = newElements_;
             this->capacity_ = capacity;
-            delete[] *newElements_;
+            delete[] newElements_;
         }
     }
 
@@ -126,41 +119,79 @@ public:
         return this->at(index);
     }
 
+    // First element of the array
+    T &front()
+    {
+        return at(0);
+    }
+
+    // Last element in the array
+    T &end()
+    {
+        auto lastIndex = this->size_ - 1;
+        return at(lastIndex);
+    }
+
+
     // add an element to end of array
     T &push_back(const T &element)
     {
-
+        insert(this->size_, element);
+        return this->elements_[this->size()];
     }
 
     // remove an element from the back
     size_t pop_back()
     {
+        erase(this->size_ - 1);
+        return size();
     }
 
     // insert an element in a spot of the array
     T &insert(size_t index, const T &element)
     {
         if (this->size_ >= this->capacity_) reserve(2 * this->capacity_);
-        if (this->size_ == index) this->elements_[index] = element;
         if (this->size_ < index) throw std::range_error("Invalid index");
         for (int i = this->size_; i != index; i--)
         {
-            elements_[i + 1] = elements_[i];
+            elements_[i] = elements_[i - 1];
         }
         elements_[index] = element;
         this->size_++;
-        auto addedValue = at(index);
-        return addedValue;
+        return elements_[index];
     }
 
     // erase an element at a given index
     size_t erase(size_t index)
     {
+        if(this->size_ <= index) throw std::range_error("Invalid index");
+        this->elements_[index].~T();
+        for (int i = index; i < this->size_ - 1; i++)
+        {
+            elements_[i] = elements_[i + 1];
+        }
+        this->size_--;
+        return size();
+    }
+
+    // swap function. swap the values of 2 elements with given index
+    void swap(size_t index1, size_t index2)
+    {
+        auto value1 = this->at(index1);
+        auto value2 = this->at(index2);
+        this->set(index1, value2);
+        this->set(index2, value1);
     }
 
     //
     void clear()
     {
+        for (int i = 0; i < this->size_; i++)
+        {
+            elements_[i].~T();
+        }
+        this->size_ = 0;
+        this->capacity_ = 0;
     }
 
 private:
@@ -179,5 +210,9 @@ private:
     // Helper function for copying array
     void copyOther(const MyVector &other)
     {
+        for (int i = 0; i < other.size_; i++)
+        {
+            this->elements_[i] = other[i];
+        }
     }
 };
--- main.cpp
+++ main.cpp
@@ -5,9 +5,44 @@ using namespace std;
 
 int main()
 {
-    MyVector<int> v1;
-    auto cap = v1.capacity();
-    cout << cap << endl;
+    MyVector<int> v1 = {};
+    v1.push_back(1);
+    v1.push_back(2);
+    v1.push_back(3);
+    v1.push_back(4);
+    MyVector<int> v2(v1);
+    v1.insert(1, 5);
+    v2.push_back(10);
+    v1.pop_back();
+    v1.erase(1);
+    MyVector<int> v3 = v1;
+    v1.push_back(8);
+
+    
+    cout << v1.capacity() << endl;
     cout << v1.size() << endl;
+    for (int i = 0; i < v1.size(); i++)
+    {
+        cout << v1[i] << endl;
+    }
+    cout << v2.capacity() << endl;
+    cout << v2.size() << endl;
+    for (int i = 0; i < v2.size(); i++)
+    {
+        cout << v2[i] << endl;
+    }
+    cout << v3.capacity() << endl;
+    cout << v3.size() << endl;
+    for (int i = 0; i < v3.size(); i++)
+    {
+        cout << v3[i] << endl;
+    }
+
+
+    v1.swap(0, v1.size() - 1);
+    cout << v1.front() << endl;
+    cout << v1.end() << endl;
+
+
     return 0;
 }
